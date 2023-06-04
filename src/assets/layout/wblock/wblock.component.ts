127cdeb2fd11141ffb207542d3fc41b82601c515
tolriaz <zjaekrxj7@gmail.com>
2023-06-04 09:15:00 +0000

    add css and js

--- src/assets/layout/wblock/wblock.component.ts
+++ src/assets/layout/wblock/wblock.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import grapesjs from 'grapesjs';
-// import 'grapesjs-preset-webpage';
+import 'grapesjs-preset-webpage';
 
 @Component({
   selector: 'wblock',
   templateUrl: './wblock.component.html',
-  styleUrls: ['./wblock.component.scss']
+  styleUrls: ['./wblock.component.scss'],
 })
 export class WblockComponent implements OnInit {
   public editor:any = null
@@ -14,27 +14,21 @@ export class WblockComponent implements OnInit {
 
   ngOnInit(): void {
     this.editor = grapesjs.init({
-      // Indicate where to init the editor. You can also pass an HTMLElement
-      container: '#gjs',
-      // Get the content for the canvas directly from the element
-      // As an alternative we could use: `components: '<h1>Hello World Component!</h1>'`,
+      height: '100%',
+      container: '#pf-web-builder',
       fromElement: true,
-      // Size of the editor
-      height: 'auto',
-      width: 'auto',
-      // Disable the storage manager for the moment
       storageManager: false,
-      // Avoid any default panel
-      panels: { defaults: [] },
+      showOffsets: true,
+      noticeOnUnload: false,
       plugins: ['gjs-preset-webpage'],
       pluginsOpts: {
-        'gjs-preset-webpage': {
-          blocksBasicOpts: {
+       'gjs-preset-webpage': {
+         blocksBasicOpts: {
            blocks: ['column2', 'column3', 'column3-7', 'text', 'link', 'image', 'video'],
            flexGrid: 1,
-          }
-        }
-      },
+         },
+       }
+      }
     })
   }
 }
