6e3f542143ce6344e38a4e9cd72401841e9d64f6
tolriaz <zjaekrxj7@gmail.com>
2023-06-07 12:18:47 +0000

    add block, video, button else

--- src/assets/layout/wbuilder/wbuilder.component.ts
+++ src/assets/layout/wbuilder/wbuilder.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import grapesjs from 'grapesjs';
-import "toastr"
+// import "toastr"
 
 @Component({
   selector: 'wbuilder',
@@ -14,10 +14,15 @@ export class WbuilderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    // Init editor
     this.editor = grapesjs.init({
-      container: '#gjs',
+      showOffsets: 1,
+      noticeOnUnload: 0,
+      container: '#web-builder',
       height: '100%',
       fromElement: true,
+      storageManager: { autoload: 0 },
       styleManager : {
         sectors: [{
             name: 'General',
@@ -46,158 +51,63 @@ export class WbuilderComponent implements OnInit {
           }
         ],
       },
-    })
-
-    this.editor.I18n.addMessages({
-      en: {
-        styleManager: {
-          properties: {
-            'background-repeat': 'Repeat',
-            'background-position': 'Position',
-            'background-attachment': 'Attachment',
-            'background-size': 'Size',
-          }
-        },
-      }
     });
 
-      var pn = this.editor.Panels;
-      var modal = this.editor.Modal;
-      var cmdm = this.editor.Commands;
+    this.editor.BlockManager.add('New Block', {
+      label: 'Block',
+      content: `<div style="padding-top:50px; padding-bottom:50px; text-align:center">New block</div>`
+    })
 
-      // Update canvas-clear command
-      cmdm.add('canvas-clear', () => {
-        if(confirm('Are you sure to clean the canvas?')) {
-          this.editor.runCommand('core:canvas-clear')
-          setTimeout(function(){ localStorage.clear()}, 0)
-        }
-      });
+    this.editor.BlockManager.add('New Text', {
+      label: 'Text',
+      content: `<p>New text</p>`
+    })
 
-      // Add info command
-      var mdlClass = 'gjs-mdl-dialog-sm';
-      var infoContainer = document.getElementById('info-panel');
+    this.editor.BlockManager.add('New Button', {
+      label: 'Button',
+      content: `<button style="padding: 1rem; text-align:center">New button</button>`
+    })
 
-      cmdm.add('open-info', () => {
-        var mdlDialog = document.querySelector('.gjs-mdl-dialog');
-        mdlDialog ? mdlDialog.className += ' ' + mdlClass : null;
-        infoContainer ? infoContainer.style.display = 'block' : null;
-        modal.setTitle('About this demo');
-        modal.setContent(infoContainer);
-        modal.open();
-        modal.getModel().once('change:open', function() {
-          mdlDialog ? mdlDialog.className = mdlDialog.className.replace(mdlClass, '') : null;
-        })
-      });
+    // this.editor.BlockManager.add('New Icon', {
+    //   label: 'Icon',
+    //   content: `<i class="fa-sharp fa-solid fa-image"></i>`
+    // })
 
-      pn.addButton('options', {
-        id: 'open-info',
-        className: 'fa fa-question-circle',
-        command: function() { this.editor.runCommand('open-info') },
-        attributes: {
-          'title': 'About',
-          'data-tooltip-pos': 'bottom',
-        },
-      });
+    // this.editor.BlockManager.add('New Link', {
+    //   label: 'Link',
+    //   content: `<button style="padding: 1rem; text-align:center">New button</div>`
+    // })
 
-      // Simple warn notifier
-      var origWarn = console.warn;
-      toastr.options = {
-        closeButton: true,
-        preventDuplicates: true,
-        showDuration: 250,
-        hideDuration: 150
-      };
-      console.warn = function (msg) {
-        if (msg.indexOf('[undefined]') == -1) {
-          toastr.warning(msg);
-        }
-        origWarn(msg);
-      };
+    this.editor.BlockManager.add('New Image', {
+      label: 'Image',
+      content: `<img src="/assets/media/icon.png">`
+    })
 
+    // this.editor.BlockManager.add('New Shape', {
+    //   label: 'Shape',
+    //   content: `<button style="padding: 1rem; text-align:center">New button</div>`
+    // })
 
-      // Add and beautify tooltips
-      [['sw-visibility', 'Show Borders'], ['preview', 'Preview'], ['fullscreen', 'Fullscreen'],
-       ['export-template', 'Export'], ['undo', 'Undo'], ['redo', 'Redo'],
-       ['gjs-open-import-webpage', 'Import'], ['canvas-clear', 'Clear canvas']]
-      .forEach(function(item) {
-        pn.getButton('options', item[0]).set('attributes', {title: item[1], 'data-tooltip-pos': 'bottom'});
-      });
-      [['open-sm', 'Style Manager'], ['open-layers', 'Layers'], ['open-blocks', 'Blocks']]
-      .forEach(function(item) {
-        pn.getButton('views', item[0]).set('attributes', {title: item[1], 'data-tooltip-pos': 'bottom'});
-      });
-      var titles = document.querySelectorAll('*[title]');
+    // this.editor.BlockManager.add('New Alert', {
+    //   label: 'Alert',
+    //   content: `<button style="padding: 1rem; text-align:center">New button</div>`
+    // })
 
-      for (var i = 0; i < titles.length; i++) {
-        var el = titles[i];
-        var title = el.getAttribute('title');
-        title = title ? title.trim(): '';
-        if(!title)
-          break;
-        el.setAttribute('data-tooltip', title);
-        el.setAttribute('title', '');
-      }
+    this.editor.BlockManager.add('New Video', {
+      label: 'Video',
+      content: `<video src="/assets/media/hcc.mp4"></video>`
+    })
 
-      // Store and load events
-      editor.on('storage:load', function(e) { console.log('Loaded ', e) });
-      editor.on('storage:store', function(e) { console.log('Stored ', e) });
+    // this.editor.BlockManager.add('New HTML', {
+    //   label: 'HTML',
+    //   content: `<button style="padding: 1rem; text-align:center">New button</div>`
+    // })
 
-
-      // Do stuff on load
-      editor.on('load', function() {
-        var $ = grapesjs.$;
-
-        // Show borders by default
-        pn.getButton('options', 'sw-visibility').set('active', 1);
-
-        // Show logo with the version
-        var logoCont = document.querySelector('.gjs-logo-cont');
-        document.querySelector('.gjs-logo-version').innerHTML = 'v' + grapesjs.version;
-        var logoPanel = document.querySelector('.gjs-pn-commands');
-        logoPanel.appendChild(logoCont);
-
-
-        // Load and show settings and style manager
-        var openTmBtn = pn.getButton('views', 'open-tm');
-        openTmBtn && openTmBtn.set('active', 1);
-        var openSm = pn.getButton('views', 'open-sm');
-        openSm && openSm.set('active', 1);
-
-        // Remove trait view
-        pn.removeButton('views', 'open-tm');
-
-        // Add Settings Sector
-        var traitsSector = $('<div class="gjs-sm-sector no-select">'+
-          '<div class="gjs-sm-sector-title"><span class="icon-settings fa fa-cog"></span> <span class="gjs-sm-sector-label">Settings</span></div>' +
-          '<div class="gjs-sm-properties" style="display: none;"></div></div>');
-        var traitsProps = traitsSector.find('.gjs-sm-properties');
-        traitsProps.append($('.gjs-trt-traits'));
-        $('.gjs-sm-sectors').before(traitsSector);
-        traitsSector.find('.gjs-sm-sector-title').on('click', function(){
-          var traitStyle = traitsProps.get(0).style;
-          var hidden = traitStyle.display == 'none';
-          if (hidden) {
-            traitStyle.display = 'block';
-          } else {
-            traitStyle.display = 'none';
-          }
-        });
-
-        // Open block manager
-        var openBlocksBtn = editor.Panels.getButton('views', 'open-blocks');
-        openBlocksBtn && openBlocksBtn.set('active', 1);
-
-        // Move Ad
-        $('#gjs').append($('.ad-cont'));
-      });
-
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-74284223-1', 'auto');
-      ga('send', 'pageview');
+    // this.editor.BlockManager.add('New Map', {
+    //   label: 'Map',
+    //   content: `<button style="padding: 1rem; text-align:center">New button</div>`
+    // })
 
   }
+
 }
